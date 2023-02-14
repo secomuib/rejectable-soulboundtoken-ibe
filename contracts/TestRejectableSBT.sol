a2e0f729c9a5a4af1702517fbe818e1a61c2b283
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-02-14 15:52:20 +0100

    remove enumerable interface

--- contracts/TestRejectableSBT.sol
+++ contracts/TestRejectableSBT.sol
@@ -6,12 +6,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./SBT/RejectableSBT.sol";
-import "./SBT/extensions/SBTEnumerable.sol";
 import "./SBT/extensions/SBTBurnable.sol";
 
 /// @title Test SBT
 /// @notice Soulbound token test contract
-contract TestRejectableSBT is RejectableSBT, SBTEnumerable, SBTBurnable {
+contract TestRejectableSBT is RejectableSBT, SBTBurnable {
     string private _baseTokenURI;
 
     constructor(
@@ -20,23 +19,5 @@ contract TestRejectableSBT is RejectableSBT, SBTEnumerable, SBTBurnable {
         string memory baseTokenURI_
     ) RejectableSBT(name_, symbol_) {
         _baseTokenURI = baseTokenURI_;
-    }
-
-    function _beforeTokenTransfer(
-        address,
-        address,
-        uint256
-    ) internal virtual override(RejectableSBT, SBTEnumerable) {}
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(RejectableSBT, SBTEnumerable)
-        returns (bool)
-    {
-        return
-            interfaceId == type(ISBTEnumerable).interfaceId ||
-            super.supportsInterface(interfaceId);
     }
 }
