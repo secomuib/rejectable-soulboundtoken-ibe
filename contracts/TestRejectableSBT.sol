09fdb9aeafe0c710d5913b44bfa7d28c755fc6ab
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-02-14 15:55:23 +0100

    remove SBTBurnable

--- contracts/TestRejectableSBT.sol
+++ contracts/TestRejectableSBT.sol
@@ -6,18 +6,15 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./SBT/RejectableSBT.sol";
-import "./SBT/extensions/SBTBurnable.sol";
 
 /// @title Test SBT
 /// @notice Soulbound token test contract
-contract TestRejectableSBT is RejectableSBT, SBTBurnable {
-    string private _baseTokenURI;
+contract TestRejectableSBT is RejectableSBT {
 
     constructor(
         string memory name_,
         string memory symbol_,
         string memory baseTokenURI_
     ) RejectableSBT(name_, symbol_) {
-        _baseTokenURI = baseTokenURI_;
     }
 }
