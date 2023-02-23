de13ec64d2e8497e72b73f0a10945a9d434acdd7
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-02-23 17:19:19 +0100

    add IBE public parameters to SBT

--- contracts/TestRejectableSBT.sol
+++ contracts/TestRejectableSBT.sol
@@ -10,9 +10,29 @@ import "./SBT/RejectableSBT.sol";
 /// @title Test SBT
 /// @notice Soulbound token test contract
 contract TestRejectableSBT is RejectableSBT {
+    // public parameters of the IBE algorithm
+    bytes public fieldOrder;
+    bytes public subgroupOrder;
+    bytes public pointP_x;
+    bytes public pointP_y;
+    bytes public pointPpublic_x;
+    bytes public pointPpublic_y;
+
     constructor(
         string memory name_,
         string memory symbol_,
-        string memory baseTokenURI_
-    ) RejectableSBT(name_, symbol_) {}
+        bytes memory fieldOrder_,
+        bytes memory subgroupOrder_,
+        bytes memory pointP_x_,
+        bytes memory pointP_y_,
+        bytes memory pointPpublic_x_,
+        bytes memory pointPpublic_y_
+    ) RejectableSBT(name_, symbol_) {
+        fieldOrder = fieldOrder_;
+        subgroupOrder = subgroupOrder_;
+        pointP_x = pointP_x_;
+        pointP_y = pointP_y_;
+        pointPpublic_x = pointPpublic_x_;
+        pointPpublic_y = pointPpublic_y_;
+    }
 }
