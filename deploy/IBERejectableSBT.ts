3ca4caa15901c3be4c222c2bd8a18ad25250c32d
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-03-28 15:25:33 +0200

    add initializationVector to deployment script

--- deploy/IBERejectableSBT.ts
+++ deploy/IBERejectableSBT.ts
@@ -1,6 +1,7 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { BigNumber } from "ethers";
 import CryptID from "@cryptid/cryptid-js";
+import crypto from "crypto";
 
 const func: DeployFunction = async ({
   // @ts-ignore
@@ -15,12 +16,16 @@ const func: DeployFunction = async ({
   const cryptID = await CryptID.getInstance();
   const cryptIDSetup = cryptID.setup(CryptID.SecurityLevel.LOWEST);
 
+  // generate 16 bytes of random data
+  const initializationVector = crypto.randomBytes(16);
+
   await deploy("IBERejectableSBT", {
     from: deployer,
     args: [
       "Rejectable IBE SBT",
       "RSBT1",
       deployer,
+      BigNumber.from(initializationVector).toHexString(),
       BigNumber.from(cryptIDSetup.publicParameters.fieldOrder).toHexString(),
       BigNumber.from(cryptIDSetup.publicParameters.subgroupOrder).toHexString(),
       BigNumber.from(cryptIDSetup.publicParameters.pointP.x).toHexString(),
